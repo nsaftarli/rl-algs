4959917681961c696d56481cf53edb3ed9a58e32
Nariman Saftarli <nsaftarli@ryerson.ca>
2019-03-12 23:03:47 -0400

    probs working

--- probs.py
+++ probs.py
@@ -38,7 +38,7 @@ def createStochastic(p1, p2):
             moveSelf = possNextStates[1]
             moveAdjacent = possNextStates[2]
             nAdjacent = len(moveAdjacent)
-            p3 = (1 - p1 - p2)/nAdjacent
+            p3 = (1 - p1 - p2)/2
 
             # If goal s' possible to reach, p1 specifies probability of transition
             # If goal s' possible to reach, p2 specifies probability of self-transition
@@ -46,14 +46,14 @@ def createStochastic(p1, p2):
                 if nAdjacent == 2:
                     probs[state, direction, moveGoal] = p1
                 else:
-                    probs[state, direction, moveGoal] = p1 + (1 - p1 - p2)/2
+                    probs[state, direction, moveGoal] = p1 + p3
                 probs[state, direction, state] = p2
             else:
                 # If goal s' not possible to reach, self-transition prob is p1 + p2
                 if nAdjacent == 2:
                     probs[state, direction, state] = p1 + p2
                 else:
-                    probs[state, direction, state] = p1 + p2 + (1 - p1 - p2)/2
+                    probs[state, direction, state] = p1 + p2 + p3
 
             # Do probabilities for adjacent moves
             for adj in moveAdjacent:
